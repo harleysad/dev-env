72da6f5d620f932302e53cce26b0c943924d5582
Harley S. A. Dalva <harleysad@gmail.com>
2025-05-14 18:58:45 -0300

    .

--- entrypoint.sh
+++ entrypoint.sh
@@ -28,23 +28,23 @@
 #       --log <level>                  Log level to use [possible values: trace, debug, info, warn, error, critical, off]
 # -----------------------------------------------------------------------------
 
-USER_ID=1000
-USER_NAME="Coder"
-PASSWORD=$SENHA_UBT
+# USER_ID=1000
+# USER_NAME="Coder"
+# PASSWORD=$SENHA_UBT
 
-# Verifica se já existe um usuário com o UID fornecido
-if getent passwd "$USER_ID" >/dev/null 2>&1; then
-    USER_NAME=$(getent passwd "$USER_ID" | cut -d: -f1)
-    echo "Atualizando senha do usuário $USER_NAME..."
-    echo "$USER_NAME:$PASSWORD" | chpasswd
-else
-    echo "Criando usuário $USER_NAME com UID $USER_ID..."
-    useradd -m -u $USER_ID -s /bin/bash $USER_NAME
-fi
+# # Verifica se já existe um usuário com o UID fornecido
+# if getent passwd "$USER_ID" >/dev/null 2>&1; then
+#     USER_NAME=$(getent passwd "$USER_ID" | cut -d: -f1)
+#     echo "Atualizando senha do usuário $USER_NAME..."
+#     echo "$USER_NAME:$PASSWORD" | chpasswd
+# else
+#     echo "Criando usuário $USER_NAME com UID $USER_ID..."
+#     useradd -m -u $USER_ID -s /bin/bash $USER_NAME
+# fi
 
-# Define a senha do usuário
-echo "$USER_NAME:$PASSWORD" | chpasswd
-usermod -aG sudo $USER_NAME
+# # Define a senha do usuário
+# echo "$USER_NAME:$PASSWORD" | chpasswd
+# usermod -aG sudo $USER_NAME
 
 # Executa a aplicação com o usuário especificado
 # exec su - $USER_NAME -c "/code serve-web --host 0.0.0.0 --port 80 --without-connection-token --server-base-path /base-path --server-data-dir /data-dir"
